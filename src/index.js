c2ebd97ed5196eb585918bd30e980c6597f72634
Shintaro Tanaka <sin_tanaka@jsl.co.jp>
2018-05-28 17:23:10 +0900

    add install function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import QuaggaScanner from "@src/Scanner"
 
-export const VERSION = '0.1.0';
+export const VERSION = '1.0.0';
 
+export function install (Vue) {
+  Vue.component('scanner', QuaggaScanner)
+}
 export const Scanner = QuaggaScanner;
