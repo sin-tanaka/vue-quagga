9cf0483036fb94ed7d0c06d0e5c2f85b9fb17319
Shintaro Tanaka <sin_tanaka@jsl.co.jp>
2018-05-28 22:00:15 +0900

    add components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,36 @@
-import QuaggaScanner from "@src/Scanner"
+import 'webrtc-adapter'
+import QuaggaScanner from "./Scanner.vue"
 
 export const VERSION = '1.0.0';
 
-export function install (Vue) {
-  Vue.component('scanner', QuaggaScanner)
-}
 export const Scanner = QuaggaScanner;
+// Install the components
+export function install (Vue) {
+  Vue.component('quagga-scanner', QuaggaScanner);
+}
+
+// Expose the components
+export { QuaggaScanner }
+
+/* -- Plugin definition & Auto-install -- */
+/* You shouldn't have to modify the code below */
+
+// Plugin
+const plugin = {
+  /* eslint-disable no-undef */
+  version: VERSION,
+  install,
+}
+
+export default plugin
+
+// Auto-install
+let GlobalVue = null
+if (typeof window !== 'undefined') {
+  GlobalVue = window.Vue
+} else if (typeof global !== 'undefined') {
+  GlobalVue = global.Vue
+}
+if (GlobalVue) {
+  GlobalVue.use(plugin)
+}
