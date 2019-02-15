1e512b234765e523754967eb826b981cbd4c0b81
Shintaro Tanaka <sin_tanaka@jsl.co.jp>
2019-02-15 20:08:19 +0900

    Add webpack.prod.js and build modules

---
+++ webpack.prod.js
@@ -0,0 +1,58 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const vueLoaderPlugin = new VueLoaderPlugin();
+
+
+module.exports = {
+  mode: 'production',
+
+  entry: {
+    'vue-quagga': path.join(__dirname, './src/index.js'),
+  },
+
+  output: {
+    path: path.join(__dirname, './dist'),
+    filename: '[name].js'
+  },
+
+  plugins: [
+    vueLoaderPlugin
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ],
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]?[hash]'
+        }
+      }
+    ]
+  },
+
+  devtool: 'source-map',
+
+  optimization: {
+    minimize: true,
+  },
+
+};
