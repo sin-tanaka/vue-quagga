7213111cd4560b54d692bc602096dcb95067e523
maffmeier <2385541+maffmeier@users.noreply.github.com>
2020-07-29 21:10:47 +0200

    Add facingMode to props

--- src/Scanner.vue
+++ src/Scanner.vue
@@ -82,6 +82,7 @@ export default {
       }),
       validator: o => typeof o.min === 'number' && typeof o.max === 'number',
     },
+    facingMode: 'environment'
   },
   data: function() {
     return {
@@ -91,7 +92,7 @@ export default {
           constraints: {
             width: { min: this.readerSize.width },
             height: { min: this.readerSize.height },
-            facingMode: 'environment',
+            facingMode: this.facingMode,
             aspectRatio: { min: 1, max: 2 },
           },
         },
